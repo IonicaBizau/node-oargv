53167b01088660d69494d6288f15a388a32a7133
Ionică Bizău <bizauionica@yahoo.com>
2015-01-17 18:48:52 +0200

    Added tests.

---
+++ test/index.js
@@ -0,0 +1,16 @@
+// Dependencies
+var OArgv = require("../lib");
+
+// Tests
+var tests = [
+    [{ r: true, _: ["target.zip", "somedir"]}, "zip"]
+  , [{ d: "http://ionicabizau.net", tt: true, size: "600x800" }, "bat"]
+  , [{ escaping: "She said: \"Hello World\"!" }, "foo"]
+  , [{ noCommand: "foo", b: true }]
+];
+
+// Run tests
+tests.forEach(function (c, i) {
+    console.log(">> Test " + i);
+    console.log("  " + JSON.stringify(c) + " ---> " + OArgv.apply(this, c));
+});
