f5cc3e84e6363648918997ed58f767507f5cbe04
Ionică Bizău <bizauionica@yahoo.com>
2015-06-18 12:10:35 +0300

    Added tests

--- test/index.js
+++ test/index.js
@@ -1,17 +1,43 @@
 // Dependencies
-var OArgv = require("../lib");
+var OArgv = require("../lib")
+  , Assert = require("assert")
+  ;
 
-// Tests
-var tests = [
-    [{ r: true, _: ["target.zip", "somedir"]}, "zip"]
-  , [{ d: "http://ionicabizau.net", tt: true, size: "600x800" }, "bat"]
-  , [{ escaping: "She said: \"Hello World\"!" }, "foo"]
-  , [{ noCommand: "foo", b: true }]
-  , [{ __: "=", custom: "Separator"}, "foo"]
-];
+it("should support commands and args", function (cb) {
+    Assert.equal(OArgv({
+        r: true
+      , _: ["target.zip", "somedir"]
+    }, "zip"), "zip -r \"target.zip\" \"somedir\"");
+    cb();
+});
 
-// Run tests
-tests.forEach(function (c, i) {
-    console.log(">> Test " + i);
-    console.log("  " + JSON.stringify(c) + " ---> " + OArgv.apply(this, c));
+it("should support boolean args", function (cb) {
+    Assert.equal(OArgv({
+        d: "http://ionicabizau.net"
+      , tt: true
+      , size: "600x800"
+    }, "bat"), "bat -d \"http://ionicabizau.net\" --tt -size \"600x800\"");
+    cb();
+});
+
+it("should escape arguments", function (cb) {
+    Assert.equal(OArgv({
+        escaping: "She said: \"Hello World\"!"
+    }, "foo"), "foo --escaping \"She said: \\\"Hello World\\\"!\"");
+    cb();
+});
+
+it("should work without commands", function (cb) {
+    Assert.equal(OArgv({
+        noCommand: "foo",
+        b: true
+    }), "--noCommand \"foo\" -b");
+    cb();
+});
+
+it("should support custom separators", function (cb) {
+    Assert.equal(OArgv({
+        __: "=",
+        custom: "Separator"
+    }, "foo"), "foo --custom=\"Separator\"")
 });
