fc17f82c0e95476aa0b0de99b3c26962fde96b69
Ionică Bizău <bizauionica@gmail.com>
2015-10-14 13:43:35 +0300

    Fixing the tests

--- test/index.js
+++ test/index.js
@@ -3,42 +3,42 @@ var OArgv = require("../lib")
   , Assert = require("assert")
   ;
 
-it("should support commands and args", function (cb) {
+it("should support commands and args and stringify the result", function (cb) {
     Assert.equal(OArgv({
         r: true
       , _: ["target.zip", "somedir"]
-    }, "zip"), "zip -r \"target.zip\" \"somedir\"");
+    }, "zip", true), "zip \"-r\" \"target.zip\" \"somedir\"");
     cb();
 });
 
-it("should support boolean args", function (cb) {
+it("should support boolean args and stringify the result", function (cb) {
     Assert.equal(OArgv({
         d: "http://ionicabizau.net"
       , tt: true
       , size: "600x800"
-    }, "bat"), "bat -d \"http://ionicabizau.net\" --tt --size \"600x800\"");
+    }, "bat", true), "bat \"-d\" \"http://ionicabizau.net\" \"--tt\" \"--size\" \"600x800\"");
     cb();
 });
 
 it("should escape arguments", function (cb) {
     Assert.equal(OArgv({
         escaping: "She said: \"Hello World\"!"
-    }, "foo"), "foo --escaping \"She said: \\\"Hello World\\\"!\"");
+    }, "foo", true), "foo \"--escaping\" \"She said: \\\"Hello World\\\"!\"");
     cb();
 });
 
 it("should work without commands", function (cb) {
-    Assert.equal(OArgv({
+    Assert.deepEqual(OArgv({
         noCommand: "foo",
         b: true
-    }), "--noCommand \"foo\" -b");
+    }), ["--noCommand", "foo", "-b"]);
     cb();
 });
 
 it("should support custom separators", function (cb) {
-    Assert.equal(OArgv({
+    Assert.deepEqual(OArgv({
         __: "=",
         custom: "Separator"
-    }, "foo"), "foo --custom=\"Separator\"")
+    }), ["--custom=\"Separator\""])
     cb();
 });
