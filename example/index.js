423d6df30fef9668f53ac0fab4d5278ef74289cc
Ionică Bizău <bizauionica@gmail.com>
2015-10-14 13:13:13 +0300

    Added array implementation
    Now, by default, the array of arguments is returned. A stringify option is also available.

--- example/index.js
+++ example/index.js
@@ -4,32 +4,32 @@ var OArgv = require("../lib");
 console.log(OArgv({
     r: true
   , _: ["target.zip", "somedir"]
-}, "zip"));
-// => zip -r "target.zip" "somedir"
+}, "zip", true));
+// => zip "-r" "target.zip" "somedir"
 
 
 console.log(OArgv({
     d: "http://ionicabizau.net"
   , tt: true
   , size: "600x800"
-}, "bat"));
-// => bat -d "http://ionicabizau.net" --tt --size "600x800"
+}, "bat", true));
+// => bat "-d" "http://ionicabizau.net" "--tt" "--size" "600x800"
 
 
 console.log(OArgv({
     escaping: "She said: \"Hello World\"!"
-}, "foo"));
-// => foo --escaping "She said: \"Hello World\"!"
+}, "foo", true));
+// => foo "--escaping" "She said: \"Hello World\"!"
 
 
 console.log(OArgv({
-    noCommand: "foo",
-    b: true
+    noCommand: "foo"
+  , b: true
 }));
-// => --noCommand "foo" -b
+// => [ '--noCommand', 'foo', '-b' ]
 
 console.log(OArgv({
     __: "=",
     custom: "Separator"
 }, "foo"));
-// => foo --custom="Separator"
+// => [ '--custom="Separator"' ]
